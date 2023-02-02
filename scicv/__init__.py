1befd6da51c5a1dcfb34ae008f83b3dff8bbc54c
Mdslauddin <mdslauddin285@gamil.com>
2023-02-02 18:43:20 +0530

    V to v version

--- scicv/__init__.py
+++ scicv/__init__.py
@@ -3,4 +3,4 @@ from ._basic import*
 from .filters import* 
 from .imgcolor import* 
 
-from .Version import __all__, __author__,__version__,__email__,__name__,__package__
+from .version import __all__, __author__,__version__,__email__,__name__,__package__
